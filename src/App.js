1fa51d52c88eb5d00267515a184ed6c296dde42e
kollyd <majaagunoyinkolade@gmail.com>
2020-04-30 17:32:05 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './Components/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
     </div>
   );
 }
